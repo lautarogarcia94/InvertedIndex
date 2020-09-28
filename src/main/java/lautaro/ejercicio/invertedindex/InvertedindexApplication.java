04e99bba8600633cd66ebbf93e6bd69f73f4efe6
Lautaro Garcia <lautygcr@gmail.com>
2020-09-27 23:51:20 -0300

    Initial Commit

---
+++ src/main/java/lautaro/ejercicio/invertedindex/InvertedindexApplication.java
@@ -0,0 +1,13 @@
+package lautaro.ejercicio.invertedindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvertedindexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvertedindexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lautaro/ejercicio/invertedindex/InvertedindexApplicationTests.java
@@ -0,0 +1,13 @@
+package lautaro.ejercicio.invertedindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvertedindexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
